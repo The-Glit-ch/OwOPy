7f38fc8aa675b52472407116c62a16c1cef84d0d
The-Glit-ch <58119800+The-Glit-ch@users.noreply.github.com>
2021-01-23 15:59:48 -0500

    Add files via upload

---
+++ Ex.py
@@ -0,0 +1,9 @@
+import OwOPy #Import OwOPy
+
+UserText = input("Normal Text Goes Here: ") #Get user input
+
+OwOText = OwOPy.owofy(UserText) #OWOFY IT OWO
+
+print(OwOText) #Show OwO text
+
+input("Hit Enter To Close: ")
---
+++ OwOPy.py
@@ -0,0 +1,3 @@
+def owofy(string:str):
+    OwO = string.lower()
+    return OwO.replace("ll","ww").replace("l","w").replace("hi","hi uwu").replace("r","w").replace(".",". o///o").replace("!","! OWO")
